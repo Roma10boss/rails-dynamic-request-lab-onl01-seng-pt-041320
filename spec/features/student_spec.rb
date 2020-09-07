f0a36821081ebaa3726635b716ed2a63fa03142f
Romario Gustave <romagustave10@gmail.com>
2020-09-07 19:38:52 +0000

    Automatically backed up by Learn

--- spec/features/student_spec.rb
+++ spec/features/student_spec.rb
@@ -29,7 +29,7 @@ describe 'Show page' do
     expect(page.status_code).to eq(200)
   end
 
-  it 'renders the first name ' do
+  it 'renders the first name in a h1 tag' do
     visit "students/#{@student.id}"
     expect(page).to have_css("h1", text: "Daenerys")
   end
